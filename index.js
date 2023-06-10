40baa374c3ed7907001cbc0369a8474915bc9878
Mdsakib999 <sakibabrar999@gmail.com>
2023-06-10 19:06:19 +0600

    added user to database

--- index.js
+++ index.js
@@ -30,8 +30,19 @@ async function run() {
     await client.connect();
 
     const teacherCollection = client.db("SummerCamp").collection("instractor");
+    const userCollection = client.db("SummerCamp").collection("users");
+
+    // All users  Api
+    app.post('/users', async(req, res) =>{
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
 
 
+
+    // instructor api
     app.get('/instractor', async(req, res) =>{
         const result = await teacherCollection.find().toArray();
         res.send(result);
